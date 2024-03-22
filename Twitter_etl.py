24e461e0da04f38efe8287e6b17b83c9e67ff62c
Kavya Madaiah <161661536+Itsme-Kavya@users.noreply.github.com>
2024-03-22 02:50:22 -0500

    Add files via upload

---
+++ Twitter_etl.py
@@ -0,0 +1,34 @@
+import tweepy
+import pandas as pd
+import json
+from datetime import datetime
+import s3fs
+from Airflow_project import Apikeys
+
+access_key = Twitter_API_Key
+access_secret = Twitter_API_secret_Key
+consumer_key = Access_Token 
+consumer_secret = Access_Token_secret
+
+
+# authentication for access
+#authorization= tweepy.OAuthHandler(access_key,access_secret)
+#authorization.set_access_token(consumer_key, consumer_secret)
+
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAFjBswEAAAAAMLsp%2FJemH7GeBETwFYIPHP5y9HY%3DuKJLGhJffm8c7TyGtCojkXPe1JTDwRrpMWLQOg1SpFZAVVJYdy"
+auth = tweepy.AppAuthHandler(consumer_key,consumer_secret)
+
+#API object
+#api = tweepy.API(authorization)
+api = tweepy.API(auth)
+#extracting tweets
+tweets = api.user_timeline(screen_name='@NVIDIAAI',
+                           # num of tweets to extract
+                           count = 100,
+                           include_rts = False, 
+                           # no retweets
+                           # for full length of tweet
+                           tweet_mode = 'extended'
+                           )
+
+print(tweets)
